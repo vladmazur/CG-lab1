67d38bd98b3d865742a2815cde3793085f37a679
Vladislav Mazur <vlad.mazur@timecode.co>
2014-03-24 10:30:42 +0200

    Init

---
+++ CG Lab1/CG Lab1/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  CG Lab1
+//
+//  Created by Влад Мазур on 3/24/14.
+//  Copyright (c) 2014 Vlad Mazur. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
